8bf1fe798732c6dff4b18a6a11a76bc89abce077
bruceeifler <bruceeiflergmail.com>
2019-07-22 15:55:04 +0000

    ho

--- squid.php
+++ squid.php
@@ -2,10 +2,10 @@
 $myXMLData =
 "<?xml version='1.0' encoding='UTF-8'?>
 <note>
-<to>Tove</to>
-<from>Jani</from>
-<heading>Reminder</heading>
-<body>Don't forget me this weekend!</body>
+<to>Buckwheat</to>
+<from>Bardoman</from>
+<heading>whodat?</heading>
+<body>Don't sneeze on the bee's knees</body>
 </note>";
 
 $xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
