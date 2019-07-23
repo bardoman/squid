5c74f964623e651bd9cf7676135fff67b2f22ab9
bruceeifler <bruceeiflergmail.com>
2019-07-23 20:08:20 +0000

    N

--- squid.php
+++ squid.php
@@ -2,29 +2,25 @@
 
 
 $xmlStr=file_get_contents("./simple.xml");
-print_r($xml);
 
 echo "<br>";
-echo "1";
+echo "Echo file contents as text=>";
 echo "<br>";
-
+echo "<br>";
 echo $xmlStr;
 
 echo "<br>";
-echo "2";
+echo "<br>";
+echo "Echo XML elements as links=>";
+echo "<br>";
 echo "<br>";
 
-$Nxml=simplexml_load_file("./simple.xml") or die("Error: Cannot create object");
-print_r($Nxml);
+$xmlElem=simplexml_load_file("./simple.xml") or die("Error: Cannot create object");
 
-echo "<br>";
-echo "3";
-echo "<br>";
-
-echo "<A HREF=",$Nxml->node[0]->URL[0]->A[0]['HREF'],">",$Nxml->node[0]->URL[0]->A,"</A><br>";
-echo "<A HREF=",$Nxml->node[1]->URL[0]->A[0]['HREF'],">",$Nxml->node[1]->URL[0]->A,"</A><br>";
-echo "<A HREF=",$Nxml->node[2]->URL[0]->A[0]['HREF'],">",$Nxml->node[2]->URL[0]->A,"</A><br>";
-echo "<A HREF=",$Nxml->node[3]->URL[0]->A[0]['HREF'],">",$Nxml->node[3]->URL[0]->A,"</A><br>";
+echo "<A HREF=",$xmlElem->node[0]->URL[0]->A[0]['HREF'],">",$xmlElem->node[0]->URL[0]->A,"</A><br>";
+echo "<A HREF=",$xmlElem->node[1]->URL[0]->A[0]['HREF'],">",$xmlElem->node[1]->URL[0]->A,"</A><br>";
+echo "<A HREF=",$xmlElem->node[2]->URL[0]->A[0]['HREF'],">",$xmlElem->node[2]->URL[0]->A,"</A><br>";
+echo "<A HREF=",$xmlElem->node[3]->URL[0]->A[0]['HREF'],">",$xmlElem->node[3]->URL[0]->A,"</A><br>";
 
 
 
