e8437de3e52490b654203ccdb0944b3426fe6f73
bruceeifler <bruceeiflergmail.com>
2019-07-23 15:53:29 +0000

    now

--- squid.php
+++ squid.php
@@ -12,6 +12,8 @@ $xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
 print_r($xml);
 
 echo "<br>";
+echo "1";
+echo "<br>";
 
 echo file_exists("./simple.xml");
 
@@ -20,19 +22,48 @@ $xmlStr=file_get_contents("./simple.xml");
 echo file_get_contents("./simple.xml");
 
 echo "<br>";
+echo "2";
+echo "<br>";
 
 $Nxml=simplexml_load_string($xmlStr) or die("Error: Cannot create object");
 print_r($Nxml);
 
 echo "<br>";
+echo "3";
+echo "<br>";
 
 $Nxml=simplexml_load_file("./simple.xml") or die("Error: Cannot create object");
 
 echo $Nxml->to . "<br>";
+echo "4";
+echo "<br>";
 echo $Nxml->from . "<br>";
+echo "5";
+echo "<br>";
 echo $Nxml->heading . "<br>";
+echo "6";
+echo "<br>";
 echo $Nxml->body . "<br>";
-echo $Nxml->URL->A ;
+echo "7";
+echo "<br>";
+echo $Nxml->URL->A . "<br>";
+echo "8";
+echo "<br>";
+
+foreach($Nxml->URL[0]->A->attributes() as $a => $b) {
+    echo $a,'="',$b,"\"\n";
+    echo "<br>";
+    echo "<A HREF=",$b,">Proverbs</A>";
+}
+
+
+
+//echo $xml->book[0]['category'] . "<br>";
+
+
+
+
+
 
 
 ?> 
