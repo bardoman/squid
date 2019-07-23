448ea07b4acaf57d532ddc5e83e1e855be6e233a
bruceeifler <bruceeiflergmail.com>
2019-07-23 20:25:13 +0000

    n

--- squid.php
+++ squid.php
@@ -22,6 +22,18 @@ echo "<A HREF=",$xmlElem->node[1]->URL[0]->A[0]['HREF'],">",$xmlElem->node[1]->U
 echo "<A HREF=",$xmlElem->node[2]->URL[0]->A[0]['HREF'],">",$xmlElem->node[2]->URL[0]->A,"</A><br>";
 echo "<A HREF=",$xmlElem->node[3]->URL[0]->A[0]['HREF'],">",$xmlElem->node[3]->URL[0]->A,"</A><br>";
 
+echo "<br>";
+echo "<br>";
+echo "Echo XML elements with loop=>";
+echo "<br>";
+echo "<br>";
+
+foreach($xmlElem->node as $node)
+{
+    echo "<A HREF=",$node->URL->A[0]['HREF'],">",$node->URL->A,"</A><br>";
+}
+
+
 
 
 
