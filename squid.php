708fbade19ee591da751e76b9e24798702ee4446
bruceeifler <bruceeiflergmail.com>
2019-07-23 19:33:27 +0000

    now

--- squid.php
+++ squid.php
@@ -1,69 +1,32 @@
 <?php
-$myXMLData =
-"<?xml version='1.0' encoding='UTF-8'?>
-<note>
-<to>Buckwheat</to>
-<from>Bardoman</from>
-<heading>whodat?</heading>
-<body>Don't sneeze on the bee's knees</body>
-</note>";
 
-$xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
+
+$xmlStr=file_get_contents("./simple.xml");
 print_r($xml);
 
 echo "<br>";
 echo "1";
 echo "<br>";
 
-echo file_exists("./simple.xml");
-
-$xmlStr=file_get_contents("./simple.xml");
-
-echo file_get_contents("./simple.xml");
+echo $xmlStr;
 
 echo "<br>";
 echo "2";
 echo "<br>";
 
-$Nxml=simplexml_load_string($xmlStr) or die("Error: Cannot create object");
+$Nxml=simplexml_load_file("./simple.xml") or die("Error: Cannot create object");
 print_r($Nxml);
 
 echo "<br>";
 echo "3";
 echo "<br>";
 
-$Nxml=simplexml_load_file("./simple.xml") or die("Error: Cannot create object");
-
-echo $Nxml->to . "<br>";
-echo "4";
-echo "<br>";
-echo $Nxml->from . "<br>";
-echo "5";
-echo "<br>";
-echo $Nxml->heading . "<br>";
-echo "6";
-echo "<br>";
-echo $Nxml->body . "<br>";
-echo "7";
-echo "<br>";
-echo $Nxml->URL->A . "<br>";
-echo "8";
-echo "<br>";
-
-foreach($Nxml->URL[0]->A->attributes() as $a => $b) {
-    echo $a,'="',$b,"\"\n";
-    echo "<br>";
-    echo "<A HREF=",$b,">Proverbs</A>";
-}
-echo "<br>";
-echo "9";
-echo "<br>";
-
-echo "<A HREF=",$Nxml->URL[0]->A[0]['HREF'],">",$Nxml->URL->A,"</A>";
+echo "<A HREF=",$Nxml->node[0]->URL[0]->A[0]['HREF'],">",$Nxml->node[0]->URL[0]->A,"</A><br>";
+echo "<A HREF=",$Nxml->node[1]->URL[0]->A[0]['HREF'],">",$Nxml->node[1]->URL[0]->A,"</A><br>";
+echo "<A HREF=",$Nxml->node[2]->URL[0]->A[0]['HREF'],">",$Nxml->node[2]->URL[0]->A,"</A><br>";
+echo "<A HREF=",$Nxml->node[3]->URL[0]->A[0]['HREF'],">",$Nxml->node[3]->URL[0]->A,"</A><br>";
 
 
-
-//echo $xml->book[0]['category'] . "<br>";
 
 
 
