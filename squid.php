39d81df625b02c88411e41eea02be6dcf2b9cb8c
beifler <beifler@us.ibm.com>
2019-07-24 14:09:25 -0600

    n

--- squid.php
+++ squid.php
@@ -1,44 +1,6 @@
 <?php
 
-/*
-$xmlStr=file_get_contents("./squid.xml");
-
-echo "<br>";
-echo "Echo file contents as text=>";
-echo "<br>";
-echo "<br>";
-echo $xmlStr;
-
-echo "<br>";
-echo "<br>";
-echo "Echo XML elements as links=>";
-echo "<br>";
-echo "<br>";
-*/
 $xmlElem=simplexml_load_file("./squid.xml") or die("Error: Cannot create object");
-/*
-echo "<A HREF=",$xmlElem->node[0]->URL[0]->A[0]['HREF'],">",$xmlElem->node[0]->URL[0]->A,"</A><br>";
-echo "<A HREF=",$xmlElem->node[1]->URL[0]->A[0]['HREF'],">",$xmlElem->node[1]->URL[0]->A,"</A><br>";
-echo "<A HREF=",$xmlElem->node[2]->URL[0]->A[0]['HREF'],">",$xmlElem->node[2]->URL[0]->A,"</A><br>";
-echo "<A HREF=",$xmlElem->node[3]->URL[0]->A[0]['HREF'],">",$xmlElem->node[3]->URL[0]->A,"</A><br>";
-
-echo "<br>";
-echo "<br>";
-echo "Echo XML elements with loop=>";
-echo "<br>";
-echo "<br>";
-
-foreach($xmlElem->node as $node)
-{
-    echo "<A HREF=",$node->URL->A[0]['HREF'],">",$node->URL->A,"</A><br>";
-}
-
-echo "<br>";
-echo "<br>";
-echo "Echo all XML elements with loop=>";
-echo "<br>";
-echo "<br>";
-*/
 
 foreach($xmlElem->node as $node)
 {
@@ -46,7 +8,7 @@ foreach($xmlElem->node as $node)
   echo "Comment=",$node->comment,"<br>";
   echo "Links=",$node->links,"<br>";
 
-  if ( !$node->task->__toString() ) {
+  if ( $node->task->asXML()==FALSE) {
     print "No task";
   }
   else
@@ -58,21 +20,10 @@ foreach($xmlElem->node as $node)
 
   echo "<br>";
 
-
     foreach($node->URL as $url)
     {
         echo "<A HREF=",$url->A[0]['HREF'],">",$url->A,"</A><br>";
     }
     echo "<br><br>";
 }
-
-
-
-
-
-
-
-
-
-
 ?> 
