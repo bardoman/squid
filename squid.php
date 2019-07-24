e395ad1d674e614d0db0f7aa29c801d8a85ce2c7
bruceeifler <bruceeiflergmail.com>
2019-07-24 16:29:57 +0000

    n

--- squid.php
+++ squid.php
@@ -43,15 +43,18 @@ echo "<br>";
 foreach($xmlElem->node as $node)
 {
  echo "Title=",$node->name,"<br>";
+  echo "Links=",$node->links,"<br>";
  echo "Task priority=",$node->task->priority,"<br>";
   echo "Task startDate=",$node->task->startDate,"<br>";
    echo "Task endDate=",$node->task->endDate,"<br>";
+
+
 
     foreach($node->URL as $url)
     {
         echo "<A HREF=",$url->A[0]['HREF'],">",$url->A,"</A><br>";
     }
-    
+    echo "<br><br>";
 }
 
 
